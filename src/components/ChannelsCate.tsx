90ad0f6de43f5b93b46e3c1fe66b97d6e62bac06
zwz <akazwz@pm.me>
2021-11-25 22:19:44 +0800

    use virtual list

--- src/components/ChannelsCate.tsx
+++ src/components/ChannelsCate.tsx
@@ -13,26 +13,76 @@ import MusicVideoIcon from '@mui/icons-material/MusicVideo'
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import StarBorder from '@mui/icons-material/StarBorder'
+import { FixedSizeList, ListChildComponentProps } from 'react-window'
+
 import { Channel } from '../pages/HomePage'
 import { Avatar, Button, Drawer, ListItemAvatar, Container, ListItem } from '@mui/material'
 
-const ChannelList = () => {
+const ChannelList = (props: { listData: any }) => {
+  const { listData } = props
   return (
     <>
       <List sx={{ minWidth: '300px' }} disablePadding>
-        <ListItemButton>
-          <ListItemAvatar>
-            <Avatar alt="A" variant="rounded" sx={{ width: '50px', height: '25px' }} />
-          </ListItemAvatar>
-          <ListItemText primary="TV" />
-        </ListItemButton>
-        <ListItemButton>
-          <ListItemAvatar>
-            <Avatar alt="A" variant="rounded" sx={{ width: '50px', height: '25px' }} />
-          </ListItemAvatar>
-          <ListItemText primary="TV" />
-        </ListItemButton>
+        {listData.map((item: Channel) => {
+          return (
+            <ListItem>
+              <ListItemButton>
+                <ListItemAvatar>
+                  {
+                    item.logo === null ?
+                      <Avatar
+                        alt={item.name ?? ''}
+                        variant="rounded"
+                        sx={{ width: 50, height: 25 }}
+                      >
+                        {item.name?.slice(0, 1)}
+                      </Avatar>
+                      :
+                      <Avatar
+                        alt={item.name ?? ''}
+                        variant="rounded"
+                        sx={{ width: 50, height: 25 }}
+                        src={item.logo}
+                      />
+                  }
+                </ListItemAvatar>
+                <ListItemText primary={item.name} sx={{ ml: '5px' }} />
+              </ListItemButton>
+            </ListItem>
+          )
+        })}
       </List>
+    </>
+  )
+}
+
+const ChannelListV = (props: { listData: any }) => {
+  const { listData } = props
+
+  const renderRow = (props: ListChildComponentProps) => {
+    const { index, style, data } = props
+    let item: Channel = data[index]
+
+    return (
+      <ListItem style={style} key={index} component="div" disablePadding>
+        <ListItemButton>
+          <ListItemText primary={`Item ${item.name}`} />
+        </ListItemButton>
+      </ListItem>
+    )
+  }
+
+  return (
+    <>
+      <FixedSizeList
+        itemSize={100}
+        itemCount={200}
+        height={400}
+        width={360}
+        itemData={listData}
+      >
+        {renderRow}
+      </FixedSizeList>
     </>
   )
 }
@@ -107,7 +157,7 @@ const ChannelsCate = (props: { cateChan: Map<any, any>, setUrl: any, setChannelN
             flexDirection: 'column',
           }}
         >
-          <ChannelList />
+          <ChannelListV listData={channelListData} />
         </Container>
       </Drawer>
     </>
