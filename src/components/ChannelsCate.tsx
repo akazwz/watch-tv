1a21059c8fd8fcec11d3a2ae11d3fed6531f4310
zwz <akazwz@pm.me>
2021-11-25 21:43:57 +0800

    start to use drawer

--- src/components/ChannelsCate.tsx
+++ src/components/ChannelsCate.tsx
@@ -14,16 +14,81 @@ import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import StarBorder from '@mui/icons-material/StarBorder'
 import { Channel } from '../pages/HomePage'
-import { Avatar, Button, Drawer, ListItemAvatar, Container } from '@mui/material'
+import { Avatar, Button, Drawer, ListItemAvatar, Container, ListItem } from '@mui/material'
 
-const ChannelCateCard = () => {
+const ChannelList = () => {
+  return (
+    <>
+      <List sx={{ minWidth: '300px' }} disablePadding>
+        <ListItemButton>
+          <ListItemAvatar>
+            <Avatar alt="A" variant="rounded" sx={{ width: '50px', height: '25px' }} />
+          </ListItemAvatar>
+          <ListItemText primary="TV" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemAvatar>
+            <Avatar alt="A" variant="rounded" sx={{ width: '50px', height: '25px' }} />
+          </ListItemAvatar>
+          <ListItemText primary="TV" />
+        </ListItemButton>
+      </List>
+    </>
+  )
+}
+
+const ChannelsCate = (props: { cateChan: Map<any, any>, setUrl: any, setChannelName: any }) => {
+  const { cateChan, setUrl, setChannelName } = props
+
   const [open, setOpen] = useState<boolean>(false)
+  const [channelListData, setChannelListData] = useState<any>([])
+
+  if (cateChan.get('Science')) {
+    const channels = cateChan.get('Science').slice(0, 1000)
+    if (channels) {
+      const element = channels.map((item: Channel) => {
+        return (
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }} onClick={() => {
+              setUrl(item.url)
+              setChannelName(item.name)
+            }}>
+              <ListItemAvatar>
+                {
+                  item.logo === null ?
+                    <Avatar
+                      alt={item.name ?? ''}
+                      variant="square"
+                      sx={{ width: 56, height: 21 }}
+                    >
+                      {item.name?.slice(0, 1)}
+                    </Avatar>
+                    :
+                    <Avatar
+                      alt={item.name ?? ''}
+                      variant="square"
+                      sx={{ width: 56, height: 21 }}
+                      src={item.logo}
+                    />
+                }
+              </ListItemAvatar>
+              <ListItemText primary={item.name} sx={{ ml: '5px' }} />
+            </ListItemButton>
+          </List>
+        )
+      })
+    }
+  }
 
   return (
     <>
       <Button
         variant="outlined"
-        onClick={() => setOpen(true)}
+        onClick={() => {
+          setOpen(true)
+          const cateChanData = cateChan.get('Music')
+          setChannelListData(cateChanData)
+        }}
       >
         Music
       </Button>
@@ -39,124 +104,12 @@ const ChannelCateCard = () => {
             display: 'flex',
             alignContent: 'center',
             alignItems: 'center',
-            flexDirection: 'column'
+            flexDirection: 'column',
           }}
         >
-          drawer
+          <ChannelList />
         </Container>
       </Drawer>
-    </>
-  )
-}
-
-const ChannelsCate = (props: { cateChan: Map<any, any>, setUrl: any, setChannelName: any }) => {
-  const { cateChan, setUrl, setChannelName } = props
-  console.log(cateChan)
-  const [openScience, setOpenScience] = useState<boolean>(false)
-  const [openAnimation, setOpenAnimation] = useState<boolean>(false)
-  const [openXXX, setOpenXXX] = useState<boolean>(false)
-
-  const [ScienceChan, setScienceChan] = useState<ReactElement>(<></>)
-  const [AnimationChan, setAnimationChan] = useState<ReactElement>(<></>)
-  const [XXXChan, setXXXChan] = useState<ReactElement>(<></>)
-
-  const setChanElement = () => {
-    if (cateChan.get('Science')) {
-      const channels = cateChan.get('Science').slice(0, 1000)
-      if (channels) {
-        const element = channels.map((item: Channel) => {
-          return (
-            <List component="div" disablePadding>
-              <ListItemButton sx={{ pl: 4 }} onClick={() => {
-                setUrl(item.url)
-                setChannelName(item.name)
-                setOpenAnimation(false)
-                setOpenXXX(false)
-              }}>
-                <ListItemAvatar>
-                  {
-                    item.logo === null ?
-                      <Avatar
-                        alt={item.name ?? ''}
-                        variant="square"
-                        sx={{ width: 56, height: 21 }}
-                      >
-                        {item.name?.slice(0, 1)}
-                      </Avatar>
-                      :
-                      <Avatar
-                        alt={item.name ?? ''}
-                        variant="square"
-                        sx={{ width: 56, height: 21 }}
-                        src={item.logo}
-                      />
-                  }
-                </ListItemAvatar>
-                <ListItemText primary={item.name} sx={{ ml: '5px' }} />
-              </ListItemButton>
-            </List>
-          )
-        })
-        setScienceChan(element)
-      }
-    }
-
-    if (cateChan.get('Animation')) {
-      const channels = cateChan.get('Animation').slice(0, 1000)
-      if (channels) {
-        const element = channels.map((item: Channel) => {
-          return (
-            <List component="div" disablePadding>
-              <ListItemButton sx={{ pl: 4 }} onClick={() => {
-                setUrl(item.url)
-                setChannelName(item.name)
-                setOpenXXX(false)
-                setOpenScience(false)
-              }}>
-                <ListItemIcon>
-                  <StarBorder />
-                </ListItemIcon>
-                <ListItemText primary={item.name} />
-              </ListItemButton>
-            </List>
-          )
-        })
-        setAnimationChan(element)
-      }
-    }
-
-    if (cateChan.get('XXX')) {
-      const channels = cateChan.get('XXX').slice(0, 1000)
-      if (channels) {
-        const element = channels.map((item: Channel) => {
-          return (
-            <List component="div" disablePadding>
-              <ListItemButton sx={{ pl: 4 }} onClick={() => {
-                setUrl(item.url)
-                setChannelName(item.name)
-                setOpenScience(false)
-                setOpenAnimation(false)
-              }}>
-                <ListItemIcon>
-                  <StarBorder />
-                </ListItemIcon>
-                <ListItemText primary={item.name} />
-              </ListItemButton>
-            </List>
-          )
-        })
-        setXXXChan(element)
-      }
-    }
-  }
-
-  useEffect(() => {
-    setChanElement()
-  }, [cateChan, setChannelName, setUrl])
-
-  return (
-    <>
-      <ChannelCateCard />
     </>
   )
 }
